40036cca602ea1d7cb8be738ee06ee0bec85d3f7
Ross Tyler <rossetyler@gmail.com>
2019-01-12 09:12:40 -0800

    COMPONENT_PATH, COMPONENT_BUILD_DIR

--- main/component.mk
+++ main/component.mk
@@ -3,7 +3,7 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-${PROJECT_PATH}/build/%: ${PROJECT_PATH}/main/%.m4; m4 $? > $@
+${COMPONENT_BUILD_DIR}/%: ${COMPONENT_PATH}/%.m4; m4 $? > $@
 
-COMPONENT_EMBED_FILES := ${PROJECT_PATH}/main/preferencesFavicon.ico ${PROJECT_PATH}/main/provisionResponseFavicon ${PROJECT_PATH}/certificates/provision_ca_cert.pem ${PROJECT_PATH}/certificates/provision_ca_key.pem
-COMPONENT_EMBED_TXTFILES := ${PROJECT_PATH}/certificates/ota_ca_cert.pem ${PROJECT_PATH}/build/preferences.html
+COMPONENT_EMBED_FILES := ${COMPONENT_PATH}/preferencesFavicon.ico ${COMPONENT_PATH}/provisionResponseFavicon ${PROJECT_PATH}/certificates/provision_ca_cert.pem ${PROJECT_PATH}/certificates/provision_ca_key.pem
+COMPONENT_EMBED_TXTFILES := ${PROJECT_PATH}/certificates/ota_ca_cert.pem ${COMPONENT_BUILD_DIR}/preferences.html
