3ea29ad18bd398ad4adc6fb65b64b952fe91bfc9
Ross Tyler <rossetyler@gmail.com>
2019-01-21 16:58:58 -0800

    change gamma to 1.2

--- main/ArtTask.cpp
+++ main/ArtTask.cpp
@@ -38,7 +38,7 @@ ArtTask::ArtTask(
 
     keyValueBroker	(keyValueBroker_),
 
-    gammaEncode		(2.0f),
+    gammaEncode		(1.2f),
 
     // timezone affects our notion of the localtime we present
     // forward a copy of any update to our task to make a synchronous change
