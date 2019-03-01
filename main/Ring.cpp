a1592a2f6e30aff1e3e6cc1b9d6c126536df5888
Ross Tyler <rossetyler@gmail.com>
2019-03-01 13:46:29 -0800

    refactor, comment

--- main/Ring.cpp
+++ main/Ring.cpp
@@ -316,9 +316,9 @@ void ArtTask::update() {
     static float constexpr phi		= (1.0f + std::sqrt(5.0f)) / 2.0f;
     static float constexpr sqrt2	= std::sqrt(2.0f);
 
-    Bump aShape(0.0f, phi   / 9.0f, 1.0f);	// < 6 seconds
-    Bump bShape(0.0f, sqrt2 / 9.0f, 1.0f);	// > 6 seconds
-    Bump cShape(0.0f, 1.5f  / 9.0f, 1.0f);	// = 6 seconds
+    Bump aShape(0.0f, phi   / 9.0f, 1.0f);	// < 6 seconds irrational
+    Bump bShape(0.0f, sqrt2 / 9.0f, 1.0f);	// > 6 seconds irrational
+    Bump cShape(0.0f, 1.5f  / 9.0f, 1.0f);	// = 6 seconds   rational
 
     Ramp<LEDI> aRamp {LEDI(aTail), LEDI(aMean)};
     Ramp<LEDI> bRamp {LEDI(bTail), LEDI(bMean)};
@@ -326,9 +326,10 @@ void ArtTask::update() {
 
     float secondsSinceBoot = esp_timer_get_time() / 1000000.0f;
 
-    updateLedChannels(ledChannel[0], aRamp(aShape(At(0.0f, secondsSinceBoot))));
-    updateLedChannels(ledChannel[1], bRamp(bShape(At(0.0f, secondsSinceBoot))));
-    updateLedChannels(ledChannel[2], cRamp(cShape(At(0.0f, secondsSinceBoot))));
+    LEDC::Channel (*rgb)[3] = &ledChannel[0];
+    updateLedChannels(*rgb++, aRamp(aShape(At(0.0f, secondsSinceBoot))));
+    updateLedChannels(*rgb++, bRamp(bShape(At(0.0f, secondsSinceBoot))));
+    updateLedChannels(*rgb++, cRamp(cShape(At(0.0f, secondsSinceBoot))));
 
     static size_t constexpr perimeterLength = 80;
 
