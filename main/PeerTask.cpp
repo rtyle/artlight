09aa95c40d38007361430273a169be049f88e61d
Ross Tyler <rossetyler@gmail.com>
2019-03-22 09:39:33 -0700

    improve error handling

--- main/PeerTask.cpp
+++ main/PeerTask.cpp
@@ -11,21 +11,22 @@ void PeerTask::receive() {
 	asio::buffer(receiveMessage, sizeof receiveMessage - 1),
 	receiveEndpoint,	// not modified!?!
 	[this](std::error_code error, std::size_t length){
-	    ESP_LOGI(name, "receive error %d length %u key %s value %s port %d %d",
-		error.value(), length,
-		receiveMessage,
-		receiveMessage + strlen(receiveMessage) + 1,
-		sendEndpoint.port(),
-		receiveEndpoint.port());
-	    if (!error.value()
-		    && 2 < length
-		    && sizeof receiveMessage > length) {
-		receiveMessage[length] = 0;
-		char const * key = receiveMessage;
-		size_t keySize = strlen(key) + 1;
-		char const * value = key + keySize;
-		if (length == keySize + strlen(value) + 1) {
-		    keyValueBroker.remotePublish(key, value);
+	    if (error) {
+		ESP_LOGE(name, "receive error: %s", error.message().c_str());
+	    } else {
+		if (!(2 < length && sizeof receiveMessage > length)) {
+		    ESP_LOGE(name, "receive bad length");
+		} else {
+		    receiveMessage[length] = 0;
+		    char const * key = receiveMessage;
+		    size_t keySize = strlen(key) + 1;
+		    char const * value = key + keySize;
+		    if (length != keySize + strlen(value) + 1) {
+			ESP_LOGE(name, "receive bad message");
+		    } else {
+			ESP_LOGI(name, "receive %s %s", key, value);
+			keyValueBroker.remotePublish(key, value);
+		    }
 		}
 	    }
 	    receive();
@@ -44,8 +45,8 @@ PeerTask::PeerTask(
 
     portObserver	(keyValueBroker, "_port", "16180",
 	[this](char const * value){
-	    static unsigned short constexpr min = 1023;		// privileged max
-	    static unsigned short constexpr max = 0xc000;	// ephemeral min
+	    static unsigned short constexpr min = 1023;	  // privileged max
+	    static unsigned short constexpr max = 0xc000; // ephemeral min
 	    unsigned short port = fromString<unsigned short>(value);
 	    if (min < port && port < max) {
 		io.post([this, port](){
@@ -67,9 +68,14 @@ PeerTask::PeerTask(
 		io.post([this, message, messageSize](){
 		    peer.async_send_to(asio::buffer(message, messageSize),
 			sendEndpoint,
-			[this, message](std::error_code, std::size_t){
-			    ESP_LOGI(name, "sent %s %s",
-				message, message + strlen(message) + 1);
+			[this, message](std::error_code error, std::size_t){
+			    if (error) {
+				ESP_LOGE(name, "send error: %s",
+				    error.message().c_str());
+			    } else {
+				ESP_LOGI(name, "send %s %s",
+				    message, message + strlen(message) + 1);
+			    }
 			    delete message;
 			});
 		});
