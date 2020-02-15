389bc641346f1e85efd06a6f382e0c1c19262160
Ross Tyler <rossetyler@gmail.com>
2020-02-15 15:07:53 -0800

    FoldsInRing supports uneven folding

--- main/InRing.cpp
+++ main/InRing.cpp
@@ -18,62 +18,63 @@ InRing & OrdinalsInRing::operator++() {
     return *this;
 }
 
-FoldsInRing::FoldsInRing(size_t folds_, size_t foldedSize_, size_t unfoldedSize_)
-:
-    InRing	(folds_ * (1 + foldedSize_ + unfoldedSize_), 2),
-    folds	(folds_),
-    foldedSize	(foldedSize_),
-    unfoldedSize(unfoldedSize_),
-    ringSize	(folds * (1 + 2 * foldedSize + unfoldedSize)),
-    folding	(foldedSize),
-    unfolding	(unfoldedSize),
-    upOnRing	(1),
-    downOnRing	(ringSize)
-{}
-InRing & FoldsInRing::operator++() {
-    ++ordinal;
-    inRing.clear();
-    if (folding) {
-	inRing.push_back(static_cast<float>(upOnRing) / ringSize);
-	inRing.push_back(static_cast<float>(--downOnRing) / ringSize);
-	--folding;
-    } else if (unfolding) {
-	inRing.push_back(static_cast<float>(upOnRing) / ringSize);
-	--unfolding;
-    } else {
-	upOnRing	+= foldedSize;
-	downOnRing	 = upOnRing;
-	float upInRing	 = static_cast<float>(upOnRing) / ringSize;
-	inRing.push_back(upInRing);
-	inRing.push_back(upInRing);
-	folding		= foldedSize;
-	unfolding	= unfoldedSize;
-    }
-    ++upOnRing;
-    return *this;
-}
-
 SectorsInRing::SectorsInRing(size_t sectors_, size_t const * sectorSize_)
 :
-    InRing	([](size_t count, size_t const * addend){
-		    size_t sum = 0;
-		    while (count--) sum += *addend++;
-		    return sum;
+    InRing	([](size_t count, size_t const * value){
+		    size_t result = 0;
+		    while (count--) result += *value++;
+		    return result;
 		}(sectors_, sectorSize_), 1),
     sectors	(sectors_),
     sectorSize	(sectorSize_),
     sector	(0),
     onSector	(0)
-{}
+{
+    inRing[0] = 0.0f;
+}
 InRing & SectorsInRing::operator++() {
     ++ordinal;
     ++onSector;
     while (onSector == *sectorSize) {
+	++sectorSize;
 	++sector;
 	onSector = 0;
-	++sectorSize;
     }
-    inRing[0] =
-	(sector + static_cast<float>(onSector) / *sectorSize) / sectors;
+    inRing[0] = (sector + static_cast<float>(onSector) / *sectorSize) / sectors;
+    return *this;
+}
+
+FoldsInRing::FoldsInRing(size_t sectors_, size_t const * foldedSize_, size_t const * unfoldedSize_)
+:
+    InRing	([](size_t count, size_t const * value0, size_t const * value1){
+		    size_t result = 0;
+		    while (count--) result += *value0++ + *value1++;
+		    return result;
+		}(sectors_, foldedSize_, unfoldedSize_), 2),
+    sectors	(sectors_),
+    foldedSize	(foldedSize_),
+    unfoldedSize(unfoldedSize_),
+    sector	(0),
+    onSector	(0),
+    onRingSize	(2 * *foldedSize + *unfoldedSize)
+{
+    inRing[0] = inRing[1] = 0.0f;
+}
+InRing & FoldsInRing::operator++() {
+    inRing.clear();
+    ++ordinal;
+    ++onSector;
+    while (onSector == *foldedSize + *unfoldedSize) {
+	++foldedSize;
+	++unfoldedSize;
+	onRingSize = 2 * *foldedSize + *unfoldedSize;
+	++sector;
+	onSector = 0;
+    }
+    if (onSector < *foldedSize) {
+	float place = (sector - static_cast<float>(onSector) / onRingSize) / sectors;
+	inRing.push_back(0.0f > place ? 1.0f + place : place);
+    }
+    inRing.push_back((sector + static_cast<float>(onSector) / onRingSize) / sectors);
     return *this;
 }
--- main/InRing.h
+++ main/InRing.h
@@ -35,28 +35,6 @@ public:
     InRing & operator++() override;
 };
 
-/// FoldsInRing is an input iterator (based on InRing)
-/// that generates ring positions for ordinals
-/// according to where they are with respect to evenly spaced folds.
-/// On creases, the same position is given twice.
-/// Off the crease but in a fold, the two positions that are folded together
-/// are given.
-/// Otherwise, the single position outside the fold is given.
-class FoldsInRing : public InRing {
-private:
-    size_t const	folds;
-    size_t const	foldedSize;
-    size_t const	unfoldedSize;
-    size_t const	ringSize;
-    size_t		folding;
-    size_t		unfolding;
-    size_t		upOnRing;
-    size_t		downOnRing;
-public:
-    FoldsInRing(size_t folds_, size_t foldedSize, size_t unfoldedSize = 0);
-    InRing & operator++() override;
-};
-
 /// SectorsInRing is an input iterator (based on InRing)
 /// that generates ring positions for ordinals according to the sector
 /// that they are in.
@@ -70,4 +48,24 @@ private:
 public:
     SectorsInRing(size_t sectors, size_t const * sectorSize);
     InRing & operator++();
+};
+
+/// FoldsInRing is an input iterator (based on InRing)
+/// that generates ring positions for ordinals
+/// according to where they are with respect to evenly spaced folds.
+/// On creases, the same position is given twice.
+/// Off the crease but in a fold, the two positions that are folded together
+/// are given.
+/// Otherwise, the single position outside the fold is given.
+class FoldsInRing : public InRing {
+private:
+    size_t const	sectors;
+    size_t const *	foldedSize;
+    size_t const *	unfoldedSize;
+    size_t		sector;
+    size_t		onSector;
+    size_t		onRingSize;
+public:
+    FoldsInRing(size_t folds, size_t const * foldedSize, size_t const * unfoldedSize);
+    InRing & operator++() override;
 };
