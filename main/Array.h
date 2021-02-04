11fd112ae87495dbf63cc1c807492efb93c6c979
Ross Tyler <rossetyler@gmail.com>
2021-02-03 19:54:00 -0800

    Update Array.h
    comment

--- main/Array.h
+++ main/Array.h
@@ -2,7 +2,7 @@
 
 /// Like std::array, Array describes an array by its size and data.
 /// Unlike std::array, an array of ragged Array objects can be created
-/// because their can be a common_type (the types don't depend on their sizes).
+/// because there can be a common_type (the types don't depend on their sizes).
 /// Given an array, the constructor can deduce its size and point to the data.
 template <typename T>
 struct Array {
