0e3989a6383b60567ed5514ed05ead729c68ec29
Ross Tyler <rossetyler@gmail.com>
2021-02-05 16:26:09 -0800

    APA102::LED<int16_t> encodes without loss of precision

--- main/APA102.cpp
+++ main/APA102.cpp
@@ -44,4 +44,46 @@ template <typename T> LED<T>::operator uint32_t () const
 // variant instantiations
 template LED<int>::operator uint32_t() const;
 template LED<unsigned>::operator uint32_t() const;
+
+// for encodings from LED<T> where T has more precision than uint8_t,
+// we could get more precision using the low order 5 bits in part.control
+// as it scales all the values down (using PWM) by this number over 31.
+// implement LED<int16_t> this way.
+// whites will be clipped to 0xfff and blacks will be clipped to 0.
+// values will effectively be shifted right by 4 by scaling as much as possible
+// first before shifting (which is where precision is lost).
+template <> LED<int16_t>::operator uint32_t () const {
+    constexpr int16_t	maxIn 		{0x0fff};
+    constexpr uint8_t	maxOut		{0xff};
+    constexpr unsigned	maxInClz	{__builtin_clz(maxIn)};
+    constexpr unsigned	maxOutClz	{__builtin_clz(maxOut)};
+    constexpr unsigned	shiftTotal	{maxOutClz - maxInClz};
+    int16_t color[3] {
+	*(&part.control + 3),
+	*(&part.control + 2),
+	*(&part.control + 1),
+    };
+    unsigned shiftPart {0};
+    for (auto & e: color) { if (e) {
+	if (e < 0) e = 0;		// clip blacks
+	else {
+	    unsigned shiftPart_ {0};
+	    if (e > maxIn) {
+		e = maxIn;		// clip whites
+		shiftPart_ = shiftTotal;
+	    } else {
+		shiftPart_ = maxOutClz - std::min(
+		    maxOutClz, static_cast<unsigned>(__builtin_clz(e)));
+	    }
+	    shiftPart = std::max(shiftPart, shiftPart_);
+	}
+    }}
+    uint32_t encoding {0};
+    for (auto & e: color) {
+	encoding |= (e >> shiftPart);
+	encoding <<= 8;
+    }
+    return encoding | 0b11100000 | (0b11111 >> (shiftTotal - shiftPart));
+}
+
 }
